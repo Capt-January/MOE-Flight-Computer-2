638c89b61131d14d2c2e97a02ed0cdd8edceb07e
Ranger921 <gejohn921@gmail.com>
2026-02-27 11:12:59 -0500

    rid of drivers

--- src/hardware-configs/pins.h
+++ src/hardware-configs/pins.h
@@ -12,17 +12,18 @@
 */
 
 // Pin definitions
-static constexpr int8_t cs1 = 10; // Barometer CS pin
-static constexpr int8_t cs2 = 36;  // Magnetometer 1 CS pin
-static constexpr int8_t cs3 = 37;  // Magnetometer 2 CS pin
-static constexpr int8_t cs4 = 38;  // LoRa CS pin
+static constexpr int8_t IMU_CS = 9; // IMU CS pin (using barometer CS pin for SPI communication)
+static constexpr int8_t BARO_CS = 10; // Barometer CS pin
+static constexpr int8_t MAG1_CS = 36;  // Magnetometer 1 CS pin
+static constexpr int8_t MAG2_CS = 37;  // Magnetometer 2 CS pin
+static constexpr int8_t RFM_95_CS = 38;  // LoRa CS pin
 
-static constexpr int8_t reset1 = 22; // GPS reset pin
-static constexpr int8_t reset2 = 2; // LoRa reset pin
+static constexpr int8_t GPS_RST = 22; // GPS reset pin
+static constexpr int8_t RFM_95_RST = 2; // LoRa reset pin
 
-static constexpr int8_t irq1 = 4; // LoRa IRQ pin
-static constexpr int8_t irq2 = 23; // GPS IRQ pin
-static constexpr int8_t irqA1 = 17; // Magnetometer A1 IRQ pin
-static constexpr int8_t irqA2 = 16; // Magnetometer A2 IRQ pin
-static constexpr int8_t irqB1 = 15; // Magnetometer B1 IRQ pin
-static constexpr int8_t irqB2 = 14; // Magnetometer B2 IRQ pin
+static constexpr int8_t RFM_95_INT = 4; // LoRa IRQ pin
+static constexpr int8_t GPS_INT = 23; // GPS IRQ pin
+static constexpr int8_t MAG_A1_INT = 17; // Magnetometer A1 IRQ pin
+static constexpr int8_t MAG_A2_INT = 16; // Magnetometer A2 IRQ pin
+static constexpr int8_t MAG_B1_INT = 15; // Magnetometer B1 IRQ pin
+static constexpr int8_t MAG_B2_INT = 14; // Magnetometer B2 IRQ pin
